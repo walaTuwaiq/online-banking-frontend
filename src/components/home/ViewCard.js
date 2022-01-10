b4da21274bafe1c8219062a6f53205798b79aa64
walaTuwaiq <wala.radi.rae.js@tuwaiq.edu.sa>
2022-01-10 21:31:00 +0300

    update user card

--- src/components/home/ViewCard.js
+++ src/components/home/ViewCard.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import "../../styles/ViewCard.css";
 import { useSelector } from "react-redux";
+import logo from "../../media/logo-bank.png"
 
 export default function ViewCard() {
   const [userData, setUserData] = useState([]);
@@ -35,7 +36,9 @@ export default function ViewCard() {
     <div>
       <div className="floating">
         <div className="card_body">
-          <div className="logo svg"></div>
+          <div className="card-logo">
+            <img src={logo} alt="logo"/>
+          </div>
           {userData.card && (
             <div className="card_no text">{userData.card[0].ibanNumber}</div>
           )}
